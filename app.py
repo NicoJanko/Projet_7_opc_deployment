08923d01b99ba8cf99e634fe11b33037d4eee111
Morkith <nicolas.jankovsky@gmail.com>
2023-02-17 10:44:56 +0100

    added feat_value and unitary test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from http.client import responses
 from sqlite3 import DatabaseError
+from webbrowser import BackgroundBrowser
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
@@ -15,7 +16,9 @@ threshold = 0.365
 #need to load the model, the explainer and the data to the api
 model = pickle.load(open('pipeline.pkl', 'rb'))
 DATABASE_URL = os.environ['DATABASE_URL']
+BRONZE_DATABASE_URL = os.environ['HEROKU_POSTGRESQL_BRONZE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn2 = psycopg2.connect(BRONZE_DATABASE_URL, sslmode='require')
 explainer = pickle.load(open('explainer.pkl', 'rb'))
 
 @app.route('/')
@@ -26,9 +29,23 @@ def index():
 @app.route('/test')
 def test():
     response = request.get_json()
-    client_id = response['client_id']
-    test = {'test':'OK! id : {}'.format(client_id)}
-    return jsonify(test)
+    test = response['test']
+    if test !=42:
+        #check the primary db
+        cur = conn.cursor()
+        cur.execute('SELECT "AMT_CREDIT" FROM client WHERE "SK_ID_CURR"=100001')
+        client_db = cur.fetchall()
+        #check the raw data db
+        cur2 = conn2.cursor()
+        cur2.execute('SELECT "CODE_GENDER" FROM full_data WHERE "SK_ID_CURR"=100001')
+        raw_db = cur2.fetchall()
+
+        return_test = {'Status' : 'OK', 'client_db': client_db, 'raw_data' : raw_db}
+    else: return_test = {'Status' : 'Error with request'}
+    return jsonify(return_test)
+
+
+
 
 def get_data(ID):
     cur = conn.cursor()
@@ -66,11 +83,16 @@ def predict():
                 'expected_val' : explainer.expected_value,}
     return jsonify(response)
 
-#def explain():
-    #retrive the id from the request and pull it's data
-    #client_id = request.get_json()
-   # client_id = client_id['client_id']
-    #client_data = get_data(client_id)
-   # if client_data is None:
-       # response = 'Client inconnu'
+@app.route('/feat')
+def feat():
+    response = request.get_json()
+    client_id = response['id']
+    feat_name = response['feat']
+    cur = conn2.cursor()
+    cur.execute(f'SELECT {feat_name} FROM raw_data WHERE "SK_ID_CURR" = {client_id}')
+    raw_data = cur.fetchall()
+    col_names = [desc[0] for desc in cur.description]
+    raw_data = pd.DataFrame(raw_data, columns=col_names).set_index('SK_ID_CURR')
+    response = {'raw_data' : raw_data.to_dict()}
+    return jsonify(response)
     
